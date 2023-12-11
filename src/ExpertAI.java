3d2ede656ca625552cb817dda8889eaceec0fbc3
Akshat <akshatk1001@gmail.com>
2023-12-10 23:19:38 -0800

    ExpertAI coded to lose lol

---
+++ src/ExpertAI.java
@@ -0,0 +1,22 @@
+public class ExpertAI {
+
+    public void makeMove(){
+        int pileSize = playGame.board.pileSize;
+        int maxRemovable = playGame.board.maxRemovable();
+        int amountToRemove;
+        
+        if (pileSize == 2) {
+            amountToRemove = 1; 
+        } 
+        else {
+            amountToRemove = (pileSize - 2) % (maxRemovable + 1);
+            if (amountToRemove == 0 || amountToRemove > maxRemovable) {
+                amountToRemove = 1 + (int)(Math.random() * maxRemovable);
+            }
+        }
+        System.out.println(" ");
+        System.out.println("There are " + playGame.board.pileSize + " sticks currently.");
+        System.out.println("ExpertAI removes " + amountToRemove + " sticks!");
+        playGame.board.pileSize -= amountToRemove;
+    }
+}
