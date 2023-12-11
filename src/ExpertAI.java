727d5473e818d87d0d45b95e87e446ccc0258e8d
Akshat <akshatk1001@gmail.com>
2023-12-11 00:03:51 -0800

    HIP HIP HOORAY

--- src/ExpertAI.java
+++ src/ExpertAI.java
@@ -1,20 +1,16 @@
 public class ExpertAI {
 
-    public void makeMove(){
+    public void makeMove() {
         int amountToRemove;
         int pileSize = playGame.board.pileSize;
         int maxRemovable = playGame.board.maxRemovable();
+        int optimalRemaining = pileSize % (maxRemovable + 1);
+        amountToRemove = optimalRemaining == 0 ? maxRemovable : optimalRemaining - 1;
 
-        
-        if (pileSize == 2) {
-            amountToRemove = 1; 
-        } 
-        else {
-            amountToRemove = (pileSize - 2) % (maxRemovable + 1);
-            if (amountToRemove == 0 || amountToRemove > maxRemovable) {
-                amountToRemove = 1 + (int)(Math.random() * maxRemovable);
-            }
+        if (amountToRemove <= 0 || amountToRemove > maxRemovable) {
+            amountToRemove = 1;  // Default to removing one if the calculation doesn't give a valid number
         }
+        
         System.out.println(" ");
         System.out.println("There are " + playGame.board.pileSize + " sticks currently.");
         System.out.println("ExpertAI removes " + amountToRemove + " sticks!");
